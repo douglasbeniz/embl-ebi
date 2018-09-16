cf096ddac9524bb39b659e0a3de7f85f98c7a694
douglasbeniz <douglasbeniz@gmail.com>
2018-09-16 05:52:40 -0300

    Some enhancements and fixes...

--- problem-b/evidence_stats.py
+++ problem-b/evidence_stats.py
@@ -11,12 +11,11 @@ import os
 import json
 import multiprocessing as mp
 
+from argparse import ArgumentParser
 from time import localtime, sleep, strftime
 
-#FILE_NAME = '17.12_evidence_data.json'
-FILE_NAME   = 'evidence_excerpt.json'
-#FILE_NAME = 'evidence_excerpt_short.json'
-FILE_OUT    = 'results%s.csv' % strftime('_%Y%m%d_%H%S%m', localtime())
+FILE_NAME   = '17.12_evidence_data.json'
+FILE_OUT    = 'results%s.csv' % strftime('_%Y%m%d_%H%M%S', localtime())
 KEYS_PATH   = ['target.id', 'disease.id', 'scores.association_score']
 STOP_TOKEN  = '5t@p_T0k3n'
 
@@ -37,15 +36,15 @@ def listener(queue):
                 output.write(', '.join(task) + '\n')
         output.close()
     except Exception as e:
-        raise('Error on thread to write final CSV file!\n\n%s' % e)
+        raise Exception('Error on thread to write final CSV file!\n\n%s' % str(e.args[0]))
 
 """
 Process a chunk of the JSON file
 """
-def process_wrapper(chunkStart, chunkSize, queue):
+def process_wrapper(jsonFileName, chunkStart, chunkSize, queue):
     try:
         with open(FILE_OUT, 'a') as output:
-            with open(FILE_NAME, 'r') as json_file:
+            with open(jsonFileName, 'r') as json_file:
                 # Look for desired chunk
                 json_file.seek(chunkStart)
                 # Load all records of this chunk
@@ -65,7 +64,7 @@ def process_wrapper(chunkStart, chunkSize, queue):
             json_file.close()
         output.close()
     except Exception as e:
-        raise('Error while parsing and getting desired info!\n\n%s' % e)
+        raise Exception('Error while parsing and getting desired info!\n\n%s' % str(e.args[0]))
 
 """
 Split the JSON file in chunks that should be process in parallel.
@@ -89,48 +88,70 @@ def chunkify(fname, size=1024*1024):
                     break
         json_file.close()
     except Exception as e:
-        raise('Error while trying to chunkify full JSON file!\n\n%s' % e)
+        raise Exception('Error while trying to chunkify full JSON file!\n\n%s' % str(e.args[0]))
 
 
 """
 Principal method
 """
 def main():
-    # -----------------------------
-    # Maximum of cores available
-    cores = mp.cpu_count()
+    # Initialization
+    json_file = FILE_NAME
 
-    # Initialize objects
-    jobs = []
+    # Parsing input paramenters
+    parser = ArgumentParser()
 
-    manager = mp.Manager()
-    queue = manager.Queue()
-    pool = mp.Pool(cores)
+    parser.add_argument("-s", "--stats", action="store_true",
+        help="Parse JSON file gathering important info and saving them into a CSV file.")
 
-    #put listener to work first
-    watcher = pool.apply_async(listener, (queue,))
+    parser.add_argument("-f", "--file", type=str,
+        help="Inform a JSON file name to analyze, otherwise will look for the use default, \'17.12_evidence_data.json\'.")
 
-    # Prepare output, at first simply erase, if any, then open to append data
-    erase = open(FILE_OUT, 'w')
-    erase.write(', '.join(KEYS_PATH) + '\n')
-    erase.close()
+    args = parser.parse_args()
 
-    # Create jobs
-    for chunkStart, chunkSize in chunkify(FILE_NAME):
-        jobs.append(pool.apply_async(process_wrapper, (chunkStart, chunkSize, queue)))
+    if args.file:
+        try:
+            json_file = args.file
+        except Exception as e:
+            print('Error when setting file name!')
+            exit()
 
-    # Wait for all jobs to finish
-    for job in jobs:
-        job.get()
+    if args.stats:
+        # -----------------------------
+        # Maximum of cores (CPUs) available
+        cores = mp.cpu_count()
 
-    # Now we are done, kill the listener
-    queue.put(STOP_TOKEN)
+        # Initialize objects
+        jobs = []
 
-    # Clean up
-    pool.close()
+        manager = mp.Manager()
+        queue   = manager.Queue()
+        pool    = mp.Pool(cores)
 
-    while queue.qsize() > 0:
-        sleep(10)
+        #put listener to work first
+        watcher = pool.apply_async(listener, (queue,))
+
+        # Prepare output, at first simply erase, if any, then open to append data
+        erase = open(FILE_OUT, 'w')
+        erase.write(', '.join(KEYS_PATH) + '\n')
+        erase.close()
+
+        # Create jobs
+        for chunk_start, chunk_size in chunkify(json_file):
+            jobs.append(pool.apply_async(process_wrapper, (json_file, chunk_start, chunk_size, queue)))
+
+        # Wait for all jobs to finish
+        for job in jobs:
+            job.get()
+
+        # Now we are done, kill the listener
+        queue.put(STOP_TOKEN)
+
+        # Clean up
+        pool.close()
+
+        while queue.qsize() > 0:
+            sleep(10)
 
 
 if __name__ == "__main__":
